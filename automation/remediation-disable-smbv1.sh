f196c15da14a86574a7aa5f08a20eeb9124fe8ee
kenbananola <ken@lognpacific.com>
2026-02-06 18:18:02 +0800

    Update remediation-disable-smbv1.sh

--- automation/remediation-disable-smbv1.sh
+++ automation/remediation-disable-smbv1.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Disable SMBv1 by forcing SMB2 minimum
-sudo sed -i 's/^server min protocol =.*/server min protocol = SMB2/' /etc/samba/smb.conf
+# Enforce SMB signing
+sudo sed -i 's/^server signing =.*/server signing = mandatory/' /etc/samba/smb.conf || echo "server signing = mandatory" | sudo tee -a /etc/samba/smb.conf
 
-# Restart Samba service
+# Restart Samba
 sudo systemctl restart smbd 2>/dev/null
 
 # This will delete the file after you're done so it doesn't stay on the local system
-rm remediation-disable-smbv1.sh
+rm remediation-enable-smb-signing.sh
 
 # Download the script
-# wget https://raw.githubusercontent.com/kenbananola/ken-remediation-scripts/main/automation/remediation-disable-smbv1.sh
+# wget https://raw.githubusercontent.com/kenbananola/ken-remediation-scripts/main/automation/remediation-enable-smb-signing.sh
 
 # Make the script executable:
-# chmod +x remediation-disable-smbv1.sh
+# chmod +x remediation-enable-smb-signing.sh
 
 # Execute the script:
-# ./remediation-disable-smbv1.sh
+# ./remediation-enable-smb-signing.sh
