fa1d595e6a9e7cf773e5660d5fc6dc2677baa303
kenbananola <ken@lognpacific.com>
2026-02-06 20:02:42 +0800

    Create remediation-enable-smb-signing.sh

---
+++ automation/remediation-enable-smb-signing.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Remediation for Tenable Plugin ID 57608
+# Vulnerability: SMB Signing Not Required
+# Fix: Enforce mandatory SMB signing
+
+CONF="/etc/samba/smb.conf"
+
+# Remove any existing server signing entries
+sudo sed -i '/^[[:space:]]*server signing[[:space:]]*=.*/d' "$CONF"
+
+# Insert mandatory signing under [global]
+if grep -q "^\[global\]" "$CONF"; then
+    sudo sed -i '/^\[global\]/a server signing = mandatory' "$CONF"
+else
+    echo -e "\n[global]\nserver signing = mandatory" | sudo tee -a "$CONF"
+fi
+
+# Restart Samba service
+sudo systemctl restart smbd 2>/dev/null
+
+# This will delete the file after you're done so it doesn't stay on the local system
+rm remediation-enable-smb-signing.sh
+
+# Download the script
+# wget <github url of raw script>
+
+# Make the script executable:
+# chmod +x remediation-enable-smb-signing.sh
+
+# Execute the script:
+# ./remediation-enable-smb-signing.sh
